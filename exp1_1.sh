a7a6e719973e29571dfbc2554f050af71db036e4
Thaer Mutlaq <thaer@rishon.csf.technion.ac.il>
2019-05-12 01:53:43 +0300

    fix exp1_1.sh script

---
+++ exp1_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for k in 32 64
+do
+	for l in 2 4 8 16
+	do
+		let poolEvery=${l}/2 
+		./py-sbatch.sh -m hw2.experiments run-exp -n exp1_1_K${k}_L${j} --seed 42 --bs-train 128 --batches 100 --epochs 100 --early-stopping 5 --filters-per-layer ${k} --layers-per-block ${l} --pool-every ${poolEvery} --hidden-dims 100
+	done 
+done
