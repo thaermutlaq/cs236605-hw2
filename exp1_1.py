4de14bf164d55feaa8c43a32320557e99b6ada9c
Thaer Mutlaq <tmutlaq@apple.com>
2019-05-12 03:14:24 +0300

    prin command before run

--- exp1_1.py
+++ exp1_1.py
@@ -9,8 +9,9 @@ for k in [32, 64]:
 	for l in [2, 4, 6 ,8]:
 		cmd = """python -m hw2.experiments run-exp -n exp1_1_K{k}_L{l} 
 		--bs-train 128 --batches 100 --epochs 120 --early-stopping 10 --lr 0.005 
-		--filters-per-layer {k} --layers-per-block ${l} --pool-every ${poolEvery} 
+		--filters-per-layer {k} --layers-per-block {l} --pool-every {poolEvery} 
 		--hidden-dims 100""".format(k=k, l=l, poolEvery =l/2)
+		print(cmd)
 		subprocess.call(cmd, shell=True)
 
 
