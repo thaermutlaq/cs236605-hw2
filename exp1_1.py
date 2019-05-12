a744a7a8166a2e8f9e45dd644b61f926b8e4d8c4
Thaer Mutlaq <tmutlaq@apple.com>
2019-05-12 03:11:36 +0300

    adjust learning rate

--- exp1_1.py
+++ exp1_1.py
@@ -7,8 +7,8 @@ import subprocess
 
 for k in [32, 64]:
 	for l in [2, 4, 6 ,8]:
-		cmd = """-m hw2.experiments run-exp -n exp1_1_K{k}_L{l} 
-		--seed 42 --bs-train 128 --batches 100 --epochs 100 --early-stopping 5 
+		cmd = """python -m hw2.experiments run-exp -n exp1_1_K{k}_L{l} 
+		--bs-train 128 --batches 100 --epochs 120 --early-stopping 10 --lr 0.005 
 		--filters-per-layer {k} --layers-per-block ${l} --pool-every ${poolEvery} 
 		--hidden-dims 100""".format(k=k, l=l, poolEvery =l/2)
 		subprocess.call(cmd, shell=True)
