666f5fd75752d857bae3e151235278178d4e896b
Thaer Mutlaq <tmutlaq@apple.com>
2019-05-12 03:25:09 +0300

    call function directly insteas of shell exec

--- exp1_1.py
+++ exp1_1.py
@@ -4,15 +4,17 @@ import argparse
 import os
 import sys
 import subprocess
+from hw2 import experiments
 
 for k in [32, 64]:
 	for l in [2, 4, 6 ,8]:
-		cmd = """python -m hw2.experiments run-exp -n exp1_1_K{k}_L{l} 
-		--bs-train 128 --batches 100 --epochs 120 --early-stopping 10 --lr 0.005 
-		--filters-per-layer {k} --layers-per-block {l} --pool-every {poolEvery} 
-		--hidden-dims 100""".format(k=k, l=l, poolEvery =l/2)
-		print(cmd)
-		subprocess.call(cmd, shell=True)
+		experiments.run_experiment(run_name = "exp1_1_K{k}_L{l}".format(k = k, l = l), out_dir='./results', seed=None,
+                   # Training params
+                   bs_train=128, bs_test=None, batches=100, epochs=120,
+                   early_stopping=10, checkpoints=None, lr=0.005, reg=1e-3,
+                   # Model params
+                   filters_per_layer=[k], layers_per_block=l, pool_every=l/2,
+                   hidden_dims=[100], ycn=False)
 
 
 
