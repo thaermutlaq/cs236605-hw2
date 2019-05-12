55b427d23bac3a7b2cb6aaa33b189f9f333ba73d
Thaer Mutlaq <tmutlaq@apple.com>
2019-05-12 03:57:44 +0300

    fix lr

--- exp1_2.py
+++ exp1_2.py
@@ -10,8 +10,8 @@ for l in [2, 4, 8]:
 	for k in [32, 64, 128 ,258]:
 		experiments.run_experiment(run_name = "exp1_2_L{l}_L{k}".format(k = k, l = l), out_dir='./results', seed=None,
                    # Training params
-                   bs_train=128, bs_test=None, batches=100, epochs=120,
-                   early_stopping=10, checkpoints=None, lr=0.005, reg=1e-3,
+                   bs_train=128, bs_test=None, batches=100, epochs=100,
+                   early_stopping=7, checkpoints=None, lr=0.001, reg=1e-3,
                    # Model params
                    filters_per_layer=[k], layers_per_block=l, pool_every=l/2,
                    hidden_dims=[100], ycn=False)
--- exp1_3.py
+++ exp1_3.py
@@ -9,8 +9,8 @@ from hw2 import experiments
 for l in [2, 4, 6 ,8]:
 	experiments.run_experiment(run_name = "exp1_3_L{l}_K64-128-256".format(l = l), out_dir='./results', seed=None,
                    # Training params
-                   bs_train=128, bs_test=None, batches=100, epochs=120,
-                   early_stopping=7, checkpoints=None, lr=0.005, reg=1e-3,
+                   bs_train=128, bs_test=None, batches=100, epochs=100,
+                   early_stopping=7, checkpoints=None, lr=0.001, reg=1e-3,
                    # Model params
                    filters_per_layer=[54, 128, 256], layers_per_block=l, pool_every=l/2,
                    hidden_dims=[100], ycn=False)
