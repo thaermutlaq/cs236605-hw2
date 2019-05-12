5a8d29ea1c6e2c2dff88c3cb1290f6587434f807
Thaer Mutlaq <tmutlaq@apple.com>
2019-05-12 05:14:59 +0300

    change pool evry 3 layers

--- exp1_1.py
+++ exp1_1.py
@@ -13,7 +13,7 @@ for k in [32, 64]:
                    bs_train=128, bs_test=None, batches=100, epochs=100,
                    early_stopping=7, checkpoints=None, lr=0.001, reg=1e-3,
                    # Model params
-                   filters_per_layer=[k], layers_per_block=l, pool_every=2,
+                   filters_per_layer=[k], layers_per_block=l, pool_every=3,
                    hidden_dims=[100], ycn=False)
 
 
